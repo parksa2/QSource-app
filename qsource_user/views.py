58d91300fb9f74104038c4baaf4a7669489851b8
rhysing <daryianrhysing@gmail.com>
2015-11-18 16:06:47 -0500

    Votes
    Moved votes to feed

--- qsource_user/views.py
+++ qsource_user/views.py
@@ -63,7 +63,7 @@ def contact(request):
                   from_email,
                   to_email,
                   html_message=some_html_message,
-                  fail_silently=True)
+                  fail_silently=False)
     
     context = {
         "form": form,
