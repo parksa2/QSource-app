95ad715e139fcafc43fe1e741c836294e948475b
SamuelWenninger <wennis@rpi.edu>
2015-11-18 18:47:50 -0500

    Default field values for answer options

--- polls/forms.py
+++ polls/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class QuestionForm(forms.Form):
     question_text = forms.CharField(label='Question', max_length=100)
-    option1 = forms.CharField(label='Option 1', max_length=25)
-    option2 = forms.CharField(label='Option 2', max_length=25)
+    option1 = forms.CharField(label='Option 1', max_length=25, initial="Yes")
+    option2 = forms.CharField(label='Option 2', max_length=25, initial="No")
