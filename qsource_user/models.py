16585aa1fd50d72e82bf7ce3b89be6cca2073a7f
nevinl <nevinl@rpi.edu>
2015-11-22 15:54:25 -0500

    Revert "Revert "User cannot answer twice""
    This reverts commit af05efc2bf1fb68b729a4acb9358845a263f4317.

--- qsource_user/models.py
+++ qsource_user/models.py
@@ -26,10 +26,15 @@ class UserData(models.Model):
         return self.user.username
     
     def answer(self, question, choice_num):
-        newAns = QuestionsAnswered.objects.create(user = self, questionID = question.pk)
-        newAns.save()
-        question.vote(choice_num)
-        question.save()
+        asked_already = QuestionsAnswered.objects.filter(user = self, questionID = question.pk)
+        if not asked_already:
+            newAns = QuestionsAnswered.objects.create(user = self, questionID = question.pk)
+            newAns.save()
+            question.vote(choice_num)
+            question.save()
+            return True
+        else:
+            return False
         
     def ask(self, new_question_text, option1, option2):
         newQuestion = Question.objects.create(question_text = new_question_text,
