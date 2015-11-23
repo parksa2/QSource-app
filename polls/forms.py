d96d8ad496e30b76068e4f75daaa887dc1e9341b
SamuelWenninger <wennis@rpi.edu>
2015-11-23 17:26:52 -0500

    Only accepts ASCII for question and answer text

--- polls/forms.py
+++ polls/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 # Form that generates the HTML required for a user to input their question and
 # answer options.
 class QuestionForm(forms.Form):
-    question_text = forms.CharField(label='Question', max_length=100)
-    option1 = forms.CharField(label='Option 1', max_length=25, initial="Yes")
-    option2 = forms.CharField(label='Option 2', max_length=25, initial="No")
+    alphanumeric = RegexValidator(r'^[\x20-\x7E]*$',
+                                  'Only ASCII characters are allowed.')
+    question_text = forms.CharField(label='Question', max_length=100, 
+                                    validators=[alphanumeric])
+    option1 = forms.CharField(label='Option 1', max_length=25, initial="Yes",
+                              validators=[alphanumeric])
+    option2 = forms.CharField(label='Option 2', max_length=25, initial="No",
+                              validators=[alphanumeric])
